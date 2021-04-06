f8d577262fb434790fe17451edecfaa38a264ba4
tandersonf92 <tandersonf@hotmail.com>
2021-04-06 12:57:55 -0300

    limpando comentarios dos arquivos

--- semana9/aula23/src/components/PokeCard/index.js
+++ semana9/aula23/src/components/PokeCard/index.js
@@ -2,17 +2,9 @@ import React , {useEffect,useState} from "react";
 import axios from "axios";
 
 const PokeCard = (props ) => {
-  // state = {
-  //   // valor do estado que guarda infos e foto do pokemon
-  //   pokemon: {}
-  // };
-
+ 
   const [pokemon,setPokemon] = useState({})
 
-
-  // método que roda após a montagem do componente
-  // componentDidMount() {
-  // }
 
   const pegaPokemon = pokeName => {
     axios
@@ -36,34 +28,11 @@ useEffect( () => {
 },[props.pokemon])
 
 
-  // [pegaPokemon,props.pokemon]
-  // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-  // componentDidUpdate(prevProps) {
-  //   // aqui, é feita uma verificação da props anterior com a props atual.
-  //   // Caso a props anterior seja diferente da props atual,
-  //   // a função pegaPokemon é chamada.
-  //   if (prevProps.pokemon !== this.props.pokemon) {
-  //     this.pegaPokemon(this.props.pokemon);
-  //   }
-  // }
-
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
   
 
-  // setPokemon(pokemon)
-    // const pokemon =   setPokemon(pokemon)
 
 
     return (
-
-    // const pokemon =   setPokemon(pokemon)
-
-
       <div>
         <p>{pokemon.name}</p>
         <p>{pokemon.weight} Kg</p>
