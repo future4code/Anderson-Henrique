ca2f362549a36fba60f803e4d1c381c90757fc18
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-19 15:35:36 -0300

    limpeza de logs

--- semana13/aula43/src/index.ts
+++ semana13/aula43/src/index.ts
@@ -6,7 +6,6 @@ const app: Express = express()
 app.use(express.json())
 app.use(cors())
 
-
 app.get('/countries/all', (req: Request, res: Response) => {
 
     const result = countries.map(country => ({
@@ -23,6 +22,12 @@ app.get('/countries/search', (req: Request, res: Response) => {
     let name = req.query.name as string
     let capital = req.query.capital as string
     let continent = req.query.continent as string
+ 
+ 
+    if (!req.query.name && !req.query.capital && !req.query.continent || !result) {
+        res.status(400).send('Nenhum parâmetro passado ou nenhum resultado encontrado')
+    }
+ 
     if (req.query.name) {
         result = result.filter(
             country => country.name.toLocaleLowerCase().includes(name.toLowerCase() as string)
@@ -44,12 +49,6 @@ app.get('/countries/search', (req: Request, res: Response) => {
         res.status(200).send(result)
     }
 
-
-    if (!req.query.name && !req.query.capital && !req.query.continent || !result) {
-        res.status(400).send('Nenhum parâmetro passado ou nenhum resultado encontrado')
-    }
-
-
 })
 
 
@@ -58,26 +57,19 @@ app.put('/countries/edit/:id', (req: Request, res: Response) => {
         if (isNaN(Number(req.params.id))) {
             throw new Error("Id must be a number");
         }
-        // const id = req.params.id as num
-        // const body = req.body
-        // console.log('body: ', body)
         const result: country | undefined = countries.find(
             country => country.id === Number(req.params.id)
         )
-
         if (!result || !req.params.id) {
             throw new Error('Not found')
         }
         if (req.body.name) {
             countries[Number(req.params.id)].name = req.body.name
-
         }
         if (req.body.capital) {
             countries[Number(req.params.id)].capital = req.body.capital
-
         }
 
-        console.table(countries)
         res.status(200).send(result)
     } catch (error) {
         res.status(400).send({ message: error.message })
@@ -99,15 +91,11 @@ app.get('/countries/:id', (req: Request, res: Response) => {
     } catch (error) {
         res.status(400).send({ message: error.message })
     }
-
 })
-
-
 
 app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Teste no /</h1>')
 })
-
 
 app.listen(3000, () => {
     console.log('Server rodando em https://localhost:3000')
