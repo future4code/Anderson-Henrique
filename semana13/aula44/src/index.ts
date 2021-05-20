877104893d19af6f11f06afad46c4aa1d89d86d8
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-20 13:59:58 -0300

    exercicios 1 a 4 realizados sem build

---
+++ semana13/aula44/src/index.ts
@@ -0,0 +1,161 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+type User = {
+  id: number,
+  name: string,
+  email: string,
+  type: UserType,
+  age: number
+}
+
+enum UserType {
+  ADMIN = "ADMIN",
+  NORMAL = "NORMAL"
+
+}
+// Mock simulando um array de usuários no Banco de Dados
+let users: User[] = [
+  {
+    id: 1,
+    name: "Alice",
+    email: "alice@email.com",
+    type: UserType.ADMIN,
+    age: 12
+  },
+  {
+    id: 2,
+    name: "Bob",
+    email: "bob@email.com",
+    type: UserType.NORMAL,
+    age: 36
+  },
+  {
+    id: 3,
+    name: "Coragem",
+    email: "coragem@email.com",
+    type: UserType.NORMAL,
+    age: 21
+  },
+  {
+    id: 4,
+    name: "Dory",
+    email: "dory@email.com",
+    type: UserType.NORMAL,
+    age: 17
+  },
+  {
+    id: 5,
+    name: "Elsa",
+    email: "elsa@email.com",
+    type: UserType.ADMIN,
+    age: 17
+  },
+  {
+    id: 6,
+    name: "Fred",
+    email: "fred@email.com",
+    type: UserType.ADMIN,
+    age: 60
+  }
+]
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+// 1.a : Metodo GET
+// 1.b : Entidade users
+app.get('/users', (req: Request, res: Response) => {
+
+  try {
+    if (!users.length) {
+      throw new Error('Lista de usuários vazia')
+    }
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(400).send(
+      {
+        message: error.message
+      }
+    )
+  }
+})
+
+
+
+
+// 2.a : por query params, pois acredito ser o padrão neste caso, sendo facilitada a lógica.
+// b. Por validacao if(type!===algo)....
+app.get('/users', (req: Request, res: Response) => {
+  try {
+    const type = req.query.type
+    if (type !== UserType.NORMAL && type !== UserType.ADMIN) {
+      throw new Error('Types aceitos: NORMAL e ADMIN')
+    }
+    const result = users.filter((user) => user.type === type)
+    res.status(200).send({ result })
+  } catch (error) {
+    res.status(400).send({
+      message: error.message
+    })
+  }
+})
+
+// 3.a pathParams
+// 3.b . feito
+
+app.get('/users/:name', (req: Request, res: Response) => {
+  try {
+    const { name } = req.params
+    const result = users.filter(user => user.name.toLowerCase() === name.toLowerCase())
+    if (!result.length) {
+      throw new Error('Nenhum usuário encontrado.')
+    }
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(400).send({
+      message: error.message
+    })
+  }
+})
+
+//4.a Aparemente a mesma coisa, ambos criaram vários usuários iguais todas as vezes que as requests foram feitas
+// 4.b Não, pois o método put foi feito para atualizar/inserir dados sem muitas verificações, sendo mais pontual o seu uso.
+app.post('/users',(req: Request, res: Response) => {
+try{
+const {id,name,email,type,age} = req.body
+if (type !== UserType.NORMAL && type !== UserType.ADMIN) {
+  throw new Error('Types aceitos para criação de novo usuário: NORMAL e ADMIN')
+}
+
+const body = req.body
+console.log('body: ',body)
+
+const newUser:User = {
+  id,name,email,type,age
+}
+users.push(newUser)
+
+res.status(200).send({
+  message: 'usuário criado',
+  user:newUser
+})
+}catch(error){
+  res.status(400).send({
+    message: error.message
+  })
+}
+})
+
+
+
+// Para testar se o servidor está tratando os endpoints corretamente
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("pong!")
+})
+
+app.listen(3003, () => {
+  console.log('Server is running at port 3003')
+})
