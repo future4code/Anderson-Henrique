ba7d76eeb2b61569c4f7676a19b6967312e4891c
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-21 10:24:23 -0300

    projeto setado. Servidor funcionando com arquivos básicos criados

---
+++ semana13/aula45/src/index.ts
@@ -0,0 +1,33 @@
+// no index.ts:
+
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { AddressInfo } from "net";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+app.get('/ping', (req:Request,res:Response) => {
+try{
+res.status(200).send('pong')
+}catch(error){
+    res.status(400).send({
+        message:error.message
+    })
+}
+
+})
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
